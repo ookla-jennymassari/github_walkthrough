3c2793c3ab0d93cedf7a98056635f77c58bca0ad
Jenny Massari <jennymassari@ookla-mac-jennymassari.local>
2025-07-23 10:43:37 -0500

    Calculator activity added to main.

--- main.py
+++ main.py
@@ -1,3 +1,44 @@
 # Welcome to Github walktrough. 
 # In this walkthrough, we'll cover the essentials of using GitHub. 
 # You'll learn how to commit changes to your repository's main file and how to create and merge branches.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+######### Calculator ########
+
+
+## Add
+
+
+## Subtract
+
+
+## Multiply
+
+
+## Divide
+
+
+## Modulus
+
+
+## Exponent
+
+
+
+
